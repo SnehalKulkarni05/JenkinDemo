cfc16cc9d51a24d988c188f88921f777733e2395
snehalkulkarni05 <snehal.kulkani05@gmail.com>
2025-01-30 21:13:39 +0530

    stash demo

--- src/test/java/TestCase/Demo1.java
+++ src/test/java/TestCase/Demo1.java
@@ -1,17 +1,1 @@
-package TestCase;
-
-import org.testng.annotations.Test;
-
-public class Demo1 {
-	@Test
-	public void m11()
-	{
-		System.out.println("Hi");
-	}
- @Test
-public void method()
-{
-Sytem.our.prinltl("updated");
-}
-
-}
+"data added"
